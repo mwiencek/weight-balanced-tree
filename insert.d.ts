bfd4058dd8d2c5ba96d20adf85c80ea9e9cdc64d
Michael Wiencek <mwtuea@gmail.com>
2022-06-08 02:22:20 -0500

    Simplify some TypeScript definitions

--- insert.d.ts
+++ insert.d.ts
@@ -34,20 +34,12 @@ export default function insert<T>(
   onConflict?: InsertConflictHandler<T, T>,
 ): ImmutableTree<T>;
 
-export function insertIfNotExists<T>(
+type InsertHelper = <T>(
   tree: ImmutableTree<T> | null,
   value: T,
   cmp: (a: T, b: T) => number,
-): ImmutableTree<T>;
+) => ImmutableTree<T>;
 
-export function insertOrReplaceIfExists<T>(
-  tree: ImmutableTree<T> | null,
-  value: T,
-  cmp: (a: T, b: T) => number,
-): ImmutableTree<T>;
-
-export function insertOrThrowIfExists<T>(
-  tree: ImmutableTree<T> | null,
-  value: T,
-  cmp: (a: T, b: T) => number,
-): ImmutableTree<T>;
+export const insertIfNotExists: InsertHelper;
+export const insertOrReplaceIfExists: InsertHelper;
+export const insertOrThrowIfExists: InsertHelper;
--- remove.d.ts
+++ remove.d.ts
@@ -6,14 +6,5 @@ export default function remove<T>(
   cmp: (a: T, b: T) => number,
 ): ImmutableTree<T> | null;
 
-export function removeIfExists<T>(
-  tree: ImmutableTree<T> | null,
-  value: T,
-  cmp: (a: T, b: T) => number,
-): ImmutableTree<T> | null;
-
-export function removeOrThrowIfNotExists<T>(
-  tree: ImmutableTree<T> | null,
-  value: T,
-  cmp: (a: T, b: T) => number,
-): ImmutableTree<T> | null;
+export const removeIfExists: typeof remove;
+export const removeOrThrowIfNotExists: typeof remove;
