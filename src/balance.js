89df85c8922411edbf17daddbf1dad350b8b7d14
Michael Wiencek <mwtuea@gmail.com>
2025-07-04 11:56:18 -0500

    Inline rotation functions
    This doesn't improve performance at all (in Node.js), but I find it easier to
    read.

--- src/balance.js
+++ src/balance.js
@@ -12,52 +12,6 @@ export function setDelta(delta/*: number */)/*: void */ {
   DELTA = delta;
 }
 
-export function rotateLeft/*:: <T> */(tree/*: MutableTree<T> */)/*: void */ {
-  const right = tree.right;
-  /*:: invariant(right.size !== 0); */
-  const left = node(tree.left, tree.value, right.left);
-  tree.left = left;
-  tree.right = right.right;
-  tree.size = left.size + right.right.size + 1;
-  tree.value = right.value;
-}
-
-export function rotateRight/*:: <T> */(tree/*: MutableTree<T> */)/*: void */ {
-  const left = tree.left;
-  /*:: invariant(left.size !== 0); */
-  const right = node(left.right, tree.value, tree.right);
-  tree.left = left.left;
-  tree.right = right;
-  tree.size = left.left.size + right.size + 1;
-  tree.value = left.value;
-}
-
-export function rotateLeftRight/*:: <T> */(tree/*: MutableTree<T> */)/*: void */ {
-  const left = tree.left;
-  /*:: invariant(left.size !== 0); */
-  const leftRight = left.right;
-  /*:: invariant(leftRight.size !== 0); */
-  const newLeft = node(left.left, left.value, leftRight.left);
-  const newRight = node(leftRight.right, tree.value, tree.right);
-  tree.left = newLeft;
-  tree.right = newRight;
-  tree.size = newLeft.size + newRight.size + 1;
-  tree.value = leftRight.value;
-}
-
-export function rotateRightLeft/*:: <T> */(tree/*: MutableTree<T> */)/*: void */ {
-  const right = tree.right;
-  /*:: invariant(right.size !== 0); */
-  const rightLeft = right.left;
-  /*:: invariant(rightLeft.size !== 0); */
-  const newRight = node(rightLeft.right, right.value, right.right);
-  const newLeft = node(tree.left, tree.value, rightLeft.left);
-  tree.left = newLeft;
-  tree.right = newRight;
-  tree.size = newLeft.size + newRight.size + 1;
-  tree.value = rightLeft.value;
-}
-
 export function heavy(w1/*: number */, w2/*: number */)/*: boolean */ {
   return w1 > (DELTA * w2);
 }
@@ -67,16 +21,26 @@ export function balanceLeft/*:: <T> */(
 )/*: ImmutableTree<T> */ {
   const left = tree.left;
   const right = tree.right;
+  /*:: invariant(left.size !== 0); */
   /*:: invariant((left.size + right.size) >= 2); */
   if (heavy(left.size, right.size)) {
     if (
       heavy(left.right.size, right.size) ||
       heavy(left.right.size + right.size + 1, left.left.size)
     ) {
-      rotateLeftRight(tree);
+      // Perform a left-right rotation
+      const leftRight = left.right;
+      /*:: invariant(leftRight.size !== 0); */
+      tree.left = node(left.left, left.value, leftRight.left);
+      tree.right = node(leftRight.right, tree.value, right);
+      tree.value = leftRight.value;
     } else {
-      rotateRight(tree);
+      // Perform a right rotation
+      tree.left = left.left;
+      tree.right = node(left.right, tree.value, right);
+      tree.value = left.value;
     }
+    tree.size = tree.left.size + tree.right.size + 1;
   }
   return tree;
 }
@@ -86,16 +50,26 @@ export function balanceRight/*:: <T> */(
 )/*: ImmutableTree<T> */ {
   const left = tree.left;
   const right = tree.right;
+  /*:: invariant(right.size !== 0); */
   /*:: invariant((left.size + right.size) >= 2); */
   if (heavy(right.size, left.size)) {
     if (
       heavy(right.left.size, left.size) ||
       heavy(right.left.size + left.size + 1, right.right.size)
     ) {
-      rotateRightLeft(tree);
+      // Perform a right-left rotation
+      const rightLeft = right.left;
+      /*:: invariant(rightLeft.size !== 0); */
+      tree.left = node(left, tree.value, rightLeft.left);
+      tree.right = node(rightLeft.right, right.value, right.right);
+      tree.value = rightLeft.value;
     } else {
-      rotateLeft(tree);
+      // Perform a left rotation
+      tree.left = node(left, tree.value, right.left);
+      tree.right = right.right;
+      tree.value = right.value;
     }
+    tree.size = tree.left.size + tree.right.size + 1;
   }
   return tree;
 }
